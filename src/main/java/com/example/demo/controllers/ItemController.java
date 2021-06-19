3af012703deec5a8edbf9672ec4d77df5041478a
Paola <arealpef@gmail.com>
2021-06-19 17:22:35 +0200

    Project

---
+++ src/main/java/com/example/demo/controllers/ItemController.java
@@ -0,0 +1,51 @@
+package com.example.demo.controllers;
+
+import java.util.List;
+import java.util.Optional;
+
+import com.example.demo.model.persistence.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.model.persistence.Item;
+import com.example.demo.model.persistence.repositories.ItemRepository;
+
+@RestController
+@RequestMapping("/api/item")
+public class ItemController {
+
+	private static final Logger log = LoggerFactory.getLogger(UserController.class);
+
+	@Autowired
+	private ItemRepository itemRepository;
+	
+	@GetMapping
+	public ResponseEntity<List<Item>> getItems() {
+		List<Item> response = itemRepository.findAll();
+		log.info("Found {} items", response.size());
+		return ResponseEntity.ok(response);
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<Item> getItemById(@PathVariable Long id) {
+		Optional<Item> response = itemRepository.findById(id);
+		log.info("Found item with the id {}", id.toString());
+		return ResponseEntity.of(response);
+	}
+	
+	@GetMapping("/name/{name}")
+	public ResponseEntity<List<Item>> getItemsByName(@PathVariable String name) {
+		List<Item> items = itemRepository.findByName(name);
+		log.info(items == null || items.isEmpty() ? "Item not Found with name " + name : "Items found" + items.size() + "with name" + name);
+		return items == null || items.isEmpty() ? ResponseEntity.notFound().build()
+				: ResponseEntity.ok(items);
+			
+	}
+	
+}
